df4a4b566324af08246d16e952d6893a618938b4
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2025-12-09 19:33:35 +0530

    Add files

--- DAY8_29NOV/SimpleInterest.js
+++ DAY8_29NOV/SimpleInterest.js
@@ -1,6 +1,17 @@
-function simpleInterest (principalAmount,interstRateyearly,durationInyears){
+// function simpleInterest (principalAmount,interstRateyearly,durationInyears){
+//     let multiplicationResult = principalAmount * interstRateyearly * durationInyears;
+//     let simpleInterest = multiplicationResult / 100;
+// // console.log(principalAmount,interstRateyearly,durationInyears,multiplicationResult,simpleInterest);
 
+// return simpleInterest;
+// }
+// console.log(simpleInterest(1000,5,1));
+
+
+function simpleInterest ( principalAmount,interstRateyearly,durationInyears) {
+    let multiplicationResult = principalAmount * interstRateyearly*durationInyears;
+    let simpleInterestfind = multiplicationResult/100;
+    return simpleInterestfind;
 
 }
-
-console.log()
+console.log(simpleInterest(1000,5,10));
