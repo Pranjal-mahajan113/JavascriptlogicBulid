f868051f7ecde85e8cd3088498b0b86fdd3edfa5
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-01-28 18:07:48 +0530

    JS practice loops

---
+++ JS_3DAY_REVISION/1DAYJSBASIC.js
@@ -0,0 +1,25 @@
+console.log(a); // Undefined
+var a = 10;
+// console.log(a) // 10
+
+let b = 20;
+b = 60;
+console.log(b); //reassign  60
+
+let c = 90;
+let d = c;
+d = 40;
+console.log(c);
+
+console.log(undefined);
+console.log(typeof null);
+
+// --------------------------------------------------------------------------------------------
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+let z = true;
+while(z){
+  console.log("hi")
+}
