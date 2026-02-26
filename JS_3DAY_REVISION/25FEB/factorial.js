f43cae44b8a743c2f5d0948d33133940d1911f35
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-02-26 18:04:34 +0530

    slove practice questions

---
+++ JS_3DAY_REVISION/25FEB/factorial.js
@@ -0,0 +1,14 @@
+function factorial(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact = i * fact;
+  }
+  return fact;
+}
+console.log(factorial(5));
+console.log(factorial(0));
+console.log(factorial(4));
+console.log(factorial(6));
+console.log(factorial(1));
+console.log(factorial(10));
+console.log(factorial(-1));
