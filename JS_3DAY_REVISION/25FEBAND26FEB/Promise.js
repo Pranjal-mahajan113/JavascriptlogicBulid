01521ae86cffc925534298c83ae92c31a0282045
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-02-28 17:48:38 +0530

    slove practice questions

--- JS_3DAY_REVISION/25FEBAND26FEB/Promise.js
+++ JS_3DAY_REVISION/25FEBAND26FEB/Promise.js
@@ -1,4 +1,16 @@
-const USERS_API = "https://jsonplaceholder.typicode.com/users";
+// const USERS_API = "https://jsonplaceholder.typicode.com/users";
 
-const user = fetch(USERS_API);
-console.log(user)
+// const user = fetch(USERS_API);
+// console.log(user)
+const cart = ["shoes","kurta","pants"];
+const promise = createOrder(cart);
+promise.then(function(){
+    proceedTopayment(orderID);
+})
+//
+function createOrder(cart){
+    const pr = new Promise(function(resolve,reject){
+
+    })
+    return pr;
+}
